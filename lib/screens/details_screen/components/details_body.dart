ff65f8d180b133a46f307c209efafb7e56042223
YSamer <ysamer2525@gmail.com>
2022-10-30 06:17:17 +0200

    edit

--- lib/screens/details_screen/components/details_body.dart
+++ lib/screens/details_screen/components/details_body.dart
@@ -11,85 +11,87 @@ class DetailsBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Container(
-          width: double.infinity,
-          padding:
-              const EdgeInsets.symmetric(horizontal: kDefaultPadding * 1.5),
-          decoration: const BoxDecoration(
-            color: kBackgroundColor,
-            borderRadius: BorderRadius.only(
-              bottomRight: Radius.circular(50),
-              bottomLeft: Radius.circular(50),
+    return SingleChildScrollView(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            width: double.infinity,
+            padding:
+                const EdgeInsets.symmetric(horizontal: kDefaultPadding * 1.5),
+            decoration: const BoxDecoration(
+              color: kBackgroundColor,
+              borderRadius: BorderRadius.only(
+                bottomRight: Radius.circular(50),
+                bottomLeft: Radius.circular(50),
+              ),
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Center(
+                  child: ProductImage(
+                    size: size,
+                    image: product.image,
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: kDefaultPadding),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: const [
+                      ColorDot(
+                        fillColor: Color(0xFF6B6B6B),
+                        isSelected: true,
+                      ),
+                      ColorDot(
+                        fillColor: Colors.blue,
+                        isSelected: false,
+                      ),
+                      ColorDot(
+                        fillColor: Colors.red,
+                        isSelected: false,
+                      ),
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding:
+                      const EdgeInsets.symmetric(vertical: kDefaultPadding / 2),
+                  child: Text(
+                    product.title,
+                    style: Theme.of(context).textTheme.headline6,
+                  ),
+                ),
+                Text(
+                  'السعر : \$${product.price}',
+                  style: const TextStyle(
+                    fontSize: 28.0,
+                    fontWeight: FontWeight.bold,
+                    color: kSecondaryColor,
+                  ),
+                ),
+                const SizedBox(height: kDefaultPadding),
+              ],
             ),
           ),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Center(
-                child: ProductImage(
-                  size: size,
-                  image: product.image,
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(vertical: kDefaultPadding),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: const [
-                    ColorDot(
-                      fillColor: Color(0xFF6B6B6B),
-                      isSelected: true,
-                    ),
-                    ColorDot(
-                      fillColor: Colors.blue,
-                      isSelected: false,
-                    ),
-                    ColorDot(
-                      fillColor: Colors.red,
-                      isSelected: false,
-                    ),
-                  ],
-                ),
-              ),
-              Padding(
-                padding:
-                    const EdgeInsets.symmetric(vertical: kDefaultPadding / 2),
-                child: Text(
-                  product.title,
-                  style: Theme.of(context).textTheme.headline6,
-                ),
-              ),
-              Text(
-                'السعر : \$${product.price}',
-                style: const TextStyle(
-                  fontSize: 28.0,
-                  fontWeight: FontWeight.bold,
-                  color: kSecondaryColor,
-                ),
-              ),
-              const SizedBox(height: kDefaultPadding),
-            ],
-          ),
-        ),
-        Container(
-          margin: const EdgeInsets.symmetric(vertical: kDefaultPadding / 2),
-          padding: const EdgeInsets.symmetric(
-            horizontal: kDefaultPadding * 1.5,
-            vertical: kDefaultPadding / 2,
-          ),
-          child: Text(
-            product.description,
-            style: const TextStyle(
-              color: Colors.white,
-              fontSize: 19,
-              height: 1.2,
+          Container(
+            margin: const EdgeInsets.symmetric(vertical: kDefaultPadding / 2),
+            padding: const EdgeInsets.symmetric(
+              horizontal: kDefaultPadding * 1.5,
+              vertical: kDefaultPadding / 2,
             ),
-          ),
-        )
-      ],
+            child: Text(
+              product.description,
+              style: const TextStyle(
+                color: Colors.white,
+                fontSize: 19,
+                height: 1.2,
+              ),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
